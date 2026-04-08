32677db9f95a7c9b87b15c233e7a172f951dc2f9
Angi-Adema <dr.adema@gmail.com>
2026-04-08 09:18:23 -0600

    set up the project, added the Shape.java file with abstract methods surface_area() and volume()

---
+++ src/com/angiadema/abstractShapes/Shape.java
@@ -0,0 +1,11 @@
+package com.angiadema.abstractShapes;
+
+public abstract class Shape {
+	
+	// Abstract method surface_area()
+	public abstract double surface_area();
+	
+	// Abstract method volume()
+	public abstract double volume();
+
+}
